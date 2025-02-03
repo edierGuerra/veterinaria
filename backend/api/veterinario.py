f88d7b0fa846da0e07e75a550ac739d69910fc2c
Edier55 <edierguerra55@gmail.com>
2025-02-03 16:55:03 -0500

    refactor(api): agrega ruta para crear un veterinario
    - agrega ruta para registrar un veterinario
    - modifica la ruta para validar el usuario sin generar el token

--- backend/api/veterinario.py
+++ backend/api/veterinario.py
@@ -1,19 +1,30 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from schemas import VeterinarioLogin,VeterinarioSchema
+from schemas import Login,VeterinarioResponse,VeterinarioCreate
 from models import Veterinario
-from auth import encode_token
+from crud import create_veterinarian
 
 router = APIRouter(prefix="/api/v1/veterinario",tags=["veterinario"])
 
-@router.post("/token")
-def login(veterinario_data:VeterinarioLogin,db:Session = Depends(get_db)):
-    veterinario = db.query(Veterinario).filter(Veterinario.username == veterinario_data.username, Veterinario.password == veterinario_data.password).first()
-    if not veterinario: 
-        return {"message":"Usuario no encontrado o contraseña invalida"} 
-    token = encode_token({"sub":veterinario.username,"id":veterinario.id})
-    return {
-        "access_token":token,
-        "token_type":"bearer"
-        }
+@router.post("/",response_model=VeterinarioResponse)
+def valide_veterinarian(form_data:Login, db:Session = Depends(get_db)):
+    user = db.query(Veterinario).filter(Veterinario.username == form_data.username, Veterinario.password == form_data.password).first()
+    if not user: 
+        return {"message":"nombre de usuario o contraseña incorrectos"}
+    return user
+
+@router.post("/create",response_model=VeterinarioResponse)
+def create_veterinarians(form_data:VeterinarioCreate,db:Session = Depends(get_db)):
+    veterinarian = create_veterinarian(
+        db = db,
+        username = form_data.username,
+        password = form_data.password,
+        names = form_data.nombres, 
+        last_names = form_data.apellidos,
+        address = form_data.direccion, 
+        phone = form_data.telefono,
+        professional_card=form_data.tarjeta_profesional
+    )
+    print(veterinarian)
+    return veterinarian
