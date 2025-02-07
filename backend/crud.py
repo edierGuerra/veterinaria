3b9569a80a95c3a7cf6f7cbb03de1da94cd442f2
Edier55 <edierguerra55@gmail.com>
2025-02-07 12:42:44 -0500

    feat(crud): agrega función para eliminar un hitorial clinicio, agrega función para registrar una visita

--- backend/crud.py
+++ backend/crud.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
-from models import Mascota, Dueno, Veterinario
+from models import Mascota, Dueno, Veterinario, HistorialClinico
+from typing import Optional
+from datetime import datetime
 from database import SessionLocal
 # CRUD veterinarios
 def create_veterinarian(db:Session,username:str, password:str, names:str, last_names:str, address:str, phone:str, professional_card:str):
@@ -59,7 +61,6 @@ def update_pet(db:Session, id_pet:int, name:str = None, id_veterinarian:int = No
         updated_pet.id_veterinario = id_veterinarian
     return updated_pet
 
-
 def delete_pet(db:Session, id_pet:int):
     deleted_pet = db.query(Mascota).filter(Mascota.id == id_pet).first()
     db.delete(deleted_pet)
@@ -80,10 +81,11 @@ def read_owner(db:Session, id_owner:int):
     return owner
 
 def update_owner(db:Session, id_owner:int, names:str = None, last_names:str = None, address:str = None, phone:str = None, email:str = None):
-    updated_owner = db.query(Dueno).filter(Dueno.id == id_owner).first()
+    """ updated_owner = db.query(Dueno).filter(Dueno.id == id_owner).first()
     db.commit()
     db.refresh(updated_owner)
-    return updated_owner
+    return updated_owner """
+    pass
 
 def delete_owner(db:Session, id_owner:int):
     deleted_owner = db.query(Dueno).filter(Dueno.id == id_owner).first()
@@ -92,3 +94,62 @@ def delete_owner(db:Session, id_owner:int):
     db.commit()
     return {"message":"Dueño eliminada satisfactoriamente"}
 
+# VISITAS 
+# Registrar una nueva visita o historial clinico en caso de no tener
+def create_visit(
+    db:Session,
+    id_veterinarian:int,
+    id_pet:int,
+    mood:str,
+    heart_rate:int,
+    respiratory_rate:int,
+    weight:float,
+    recommendations:str,
+    temperature:float,
+    ):
+    """
+    Crea una nueva visita con los datos proporcionados.
+    """
+    #Comprobar el veterinario
+    veterinarian = db.query(Veterinario).filter(Veterinario.id == id_veterinarian).first()
+    #Comprobar la mascota
+    pet = db.query(Mascota).filter(Mascota.id == id_pet).first()
+    #Comprobar el historial clinico
+    historial_clinico = db.query(HistorialClinico).filter(HistorialClinico.mascota_id == pet.id).first()
+    #Instancia de la visita
+    visit = veterinarian.registrar_visita(
+        mascota_id=id_pet,
+        estado_animo=mood,
+        frecuencia_cardiaca=heart_rate,
+        frecuencia_respiratoria=respiratory_rate,
+        peso=weight,
+        recomendaciones=recommendations,
+        temperatura=temperature
+    )
+    #Asignar nueva visita al historial clinico en caso de que si exista
+    if historial_clinico:
+        historial_clinico.visitas.append(visit)
+        db.add(historial_clinico)
+        db.refresh(historial_clinico)
+    #Crear historial clinico en caso de que este no exista
+    if not historial_clinico:
+        nuevo_historial_clinico = HistorialClinico(
+            mascota_id = pet.id
+        )
+        #Asignar nueva visita al nuevo historial 
+        nuevo_historial_clinico.visitas.append(visit)
+        db.add(nuevo_historial_clinico)
+        db.refresh(nuevo_historial_clinico)
+    #Asignar visita al veterinario
+    veterinarian.visitas.append(visit)
+    db.add(veterinarian)
+    db.commit()# Guardar cambios
+    db.refresh(veterinarian)
+    return visit
+
+# Eliminar un historial clinico por id
+def eliminar_historial_clinico(id_mascota:int,db:Session):
+    historial = db.query(HistorialClinico).filter(HistorialClinico.mascota_id == id_mascota).first()
+    db.delete(historial)
+    db.commit()
+    return "historial eliminado exitosamente"
